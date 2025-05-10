6503a3a57a2cf95a03937591d4f57411e23d4c84
Adam Montano <amontano495@gmail.com>
2025-05-09 21:31:25 -0700

    exported functions on one line

--- src/controllers/mod.rs
+++ src/controllers/mod.rs
@@ -1,7 +1,4 @@
 pub mod app;
 
 // Re-export key functions
-pub use app::start_app;
-pub use app::init_feed;
-pub use app::create_post_via_editor;
-pub use app::post_note;
+pub use app::{start_app,init_feed,create_post_via_editor,post_note};
